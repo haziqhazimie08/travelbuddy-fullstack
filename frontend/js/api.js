b3fa3095a2c509b464173536f6fba0f1d64dc200
haziqhazimie08 <haziqhazimie08@gmail.com>
2026-06-18 22:56:29 +0800

    prod: link frontend to live production railway api

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,5 @@
-const API_BASE     = 'http://127.0.0.1:8000/api';
-const STORAGE_BASE = 'http://127.0.0.1:8000/storage';
+const API_BASE     = 'https://travelbuddy-fullstack-production.up.railway.app/api';
+const STORAGE_BASE = 'https://travelbuddy-fullstack-production.up.railway.app/storage';
 
 const api = {
     getToken: () => localStorage.getItem('tb_token'),
